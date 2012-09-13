ba5cb298e7401505c58d20cf0575050c47ad4a2b
whchen <whchen1080@gmail.com>
2012-09-13 16:15:17 +0800

    支持conf配置文件

--- translate.py
+++ translate.py
@@ -2,21 +2,26 @@
 import urllib
 import json
 from optparse import OptionParser
+import ConfigParser
 import chardet
-#有道的key，要从http://fanyi.youdao.com/openapi?path=data-mode上申请
-youdao_key = '1222489799'
-youdao_key_from = 'myblogaudoe1'
 
-def youdao_api(key,keyfrom,type='json'):
+#有道的key，要从http://fanyi.youdao.com/openapi?path=data-mode上申请
+
+def youdao_api(type='json'):
+    cf = ConfigParser.ConfigParser()
+    cf.read('youdao.conf')
+    key = cf.get('youdao','key')
+    keyfrom = cf.get('youdao','key_from')
     if key and keyfrom:
         return 'http://fanyi.youdao.com/openapi.do?keyfrom=%s&key=%s&type=data&doctype=%s&version=1.1&q='%(keyfrom,key,type)
 
 def translate(key_world):
-    base_url = youdao_api(youdao_key,youdao_key_from)
+    base_url = youdao_api()
     key_world.decode(chardet.detect(key_world)['encoding']).encode('utf8')
     json_data =urllib.urlopen(base_url+key_world)
     resault = json.load(json_data)
     return resault
+
 def print_resault(resault):
     print 'keyword:%s'%(resault['query'])
     if resault.get('basic',''):
@@ -24,6 +29,7 @@ def print_resault(resault):
         print ','.join(basic['explains'])
     else:
         print "没有这个单词的解释。请检查你的输入。"
+
 if __name__ == '__main__':
     perser = OptionParser()
     perser.add_option("-k",'--keyword',dest="keyword",help="keywork")
